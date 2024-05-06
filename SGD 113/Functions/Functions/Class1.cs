fa1a1eeaea5c5230d4d5148b80cf8ee0efd40580
nchapman6577 <73857037+nchapman6577@users.noreply.github.com>
2024-05-06 03:52:04 -0400

    Add files via upload

---
+++ SGD 113/Functions/Functions/Class1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        // Calling both functions and printing the output to the console
+        int resultMultiplyNumbers = MultiplyNumbers(34, 4);//stores results in new variable
+        Console.WriteLine($"34 * 4: {resultMultiplyNumbers}");//prints results to console with a message
+
+        int resultAddNumbers = AddNumbers(12, 22);//stores results in new variable
+        Console.WriteLine($"12 + 22: {resultAddNumbers}");//prints results to console with a message
+    }
+
+    // Function to multiply two numbers
+    public static int MultiplyNumbers(int a, int b)//names funcation and declares variables
+    {
+        return a * b;//returns the answer of two numbers being multiplied 
+    }
+
+    // Function to add two numbers
+    public static int AddNumbers(int a, int b)//names funcation and declares variables
+    {
+        return a + b;//returns the sum of two numbers
+    }
+}
---
+++ SGD 113/If Statement/If Statement/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        //ask the user a question with three options
+        Console.WriteLine("Choose your Pokemon, (Bulbasaur, Squirtle, and Charmander)");
+
+        //get input from the user
+        string userInput = Console.ReadLine();
+
+        if (userInput.Equals("Bulbasaur", StringComparison.OrdinalIgnoreCase))//user picked Bulbasaur
+        {
+            Console.WriteLine("You picked a grass type pokemon!");//print message to console
+        }
+        else if (userInput.Equals("Squirtle", StringComparison.OrdinalIgnoreCase))//user picked Squirtle
+        {
+            Console.WriteLine("You picked a water type pokemon!");//print message to console
+        }
+        else if (userInput.Equals("Charmander", StringComparison.OrdinalIgnoreCase))//user picked Charmander
+        {
+            Console.WriteLine("You picked a fire type pokemon!");//print message to console
+        }
+        else
+        {
+            Console.WriteLine("Wrong choice no pokemon for you!");//invalid input
+        }
+    }
+}
